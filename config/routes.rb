733e887cd11acd278ecc777d4670a223b206e96a
EC2 Default User <ec2-user@ip-172-31-88-72.ec2.internal>
2020-02-20 06:39:14 +0000

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'about' => 'home#about'
+  get 'member' => 'home#member'
+  get 'result' => 'home#result'
+  get 'link' => 'home#link'
+  get 'contact' => 'home#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
